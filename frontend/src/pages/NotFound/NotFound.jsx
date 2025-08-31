dcb7b1ceb007df9797dae3f4e6ecc9a1cc54916f
HalilIbrahimKlblk <halilkalabalik64@gmail.com>
2025-08-31 14:19:50 +0300

    NotFound sayfası geliştirildi.

--- frontend/src/pages/NotFound/NotFound.jsx
+++ frontend/src/pages/NotFound/NotFound.jsx
@@ -3,7 +3,12 @@ import './NotFound.css'
 
 const NotFound = () => {
     return (
-        <div>NotFound</div>
+        <div className='notFound-div'>
+            <h1 className='notFound-h1'>Üzgünüz, bu sayfaya ulaşılamıyor</h1>
+            <p className='notFound-p'>Tıkladığınız bağlantı bozuk olabilir veya sayfa kaldırılmış olabilir.</p>
+            <img className='img-eror' src='../img/notfound.png' width={250} />
+            <p><a className='notFound-a' href='/'>Ana Menüye Geri Dön</a></p>
+        </div>
     )
 }
 
