978f4b7dd82d8833f86ac990ffe856a9cf56a7a9
HalilIbrahimKlblk <halilkalabalik64@gmail.com>
2025-08-31 13:53:09 +0300

    Sayfalar arası router altyapısı oluşturuldu.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,26 @@
-import { useState } from 'react'
-import './App.css'
+import React from 'react';
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import About from './pages/About/About';
+import NotFound from './pages/NotFound/NotFound';
+import MainLayout from './layouts/MainLayout';
+import EmptyLayout from './layouts/EmptyLayout';
 
-function App() {
-
+const App = () => {
   return (
     <>
+      <BrowserRouter>
+        <Routes>
+          {/* Ana sayfa - Navbar ve Footer var */}
+          <Route path="/" element={<MainLayout><Home /></MainLayout>} />
+          <Route path="/About" element={<MainLayout><About /></MainLayout>} />
+          {/* 404 sayfası - Navbar ve Footer yok */}
+          <Route path="*" element={<EmptyLayout><NotFound /></EmptyLayout>} />
+        </Routes>
+      </BrowserRouter>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
